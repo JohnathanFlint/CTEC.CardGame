7c4435556bd05fc7d1ea101580fd5e215f402980
JohnathanFlint <nicholas.white142@gmail.com>
2016-10-25 09:45:53 -0600

    Created the card base class with associated methods and init.

--- CTEC.CardGame/Model/Card.swift
+++ CTEC.CardGame/Model/Card.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2016 CTEC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Card
+{
+    private var backImage : UIImage
+    private var isFaceUp : Bool
+    
+    init()
+    {
+        backImage = UIImage()
+        isFaceUp = false
+    }
+    
+    func toString() -> String
+    {
+        let description = "The card is \(isFaceUp)"
+        return description
+    }
+    
+    func getBackImage() -> UIImage
+    {
+        return backImage
+    }
+    
+    func isUp() -> Bool
+    {
+        return isFaceUp
+    }
+    
+}
