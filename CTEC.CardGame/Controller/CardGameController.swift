59aba288d6b427fbd4a2d84a899591b26376329c
JohnathanFlint <nicholas.white142@gmail.com>
2016-10-27 09:25:07 -0600

    Updated storyboard connections and added files.
    Linked ViewController to CardGameController, removed default view
    controller, and organized files.

---
+++ CTEC.CardGame/Controller/CardGameController.swift
@@ -0,0 +1,14 @@
+//
+//  CardGameController.swift
+//  CTEC.CardGame
+//
+//  Created by White, Nicholas on 10/27/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class CardGameController : UIViewController
+{
+    
+}
