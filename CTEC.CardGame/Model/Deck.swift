3ccf8ad134bec1752d85f169cf524c79ffd11ba5
JohnathanFlint <nicholas.white142@gmail.com>
2016-10-31 08:22:40 -0600

    Created deck and saying card deck.

---
+++ CTEC.CardGame/Model/Deck.swift
@@ -0,0 +1,14 @@
+//
+//  Deck.swift
+//  CTEC.CardGame
+//
+//  Created by White, Nicholas on 10/31/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class Deck : PlayingCard
+{
+    
+}
