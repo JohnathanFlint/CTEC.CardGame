8b922ed23f4c4faf4e9a19da71ede9994bfeb601
JohnathanFlint <nicholas.white142@gmail.com>
2016-11-11 09:05:31 -0700

    Declared variables.

--- CTEC.CardGame/Model/StupidGame.swift
+++ CTEC.CardGame/Model/StupidGame.swift
@@ -6,4 +6,34 @@
 //  Copyright © 2016 CTEC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class StupidGame
+{
+    //Decleration Section
+    internal var deck : PlayingCardDeck
+    internal var wasteBasketofFire : PlayingCardDeck
+    internal var playingCard1 : PlayingCard
+    internal var playingCard2 : PlayingCard
+    internal var hasEnded : Bool
+    internal var buttonClicked : Bool
+    
+    
+    //inits
+    init()
+    {
+        deck = PlayingCardDeck()
+        wasteBasketofFire = PlayingCardDeck()
+        playingCard1 = PlayingCard()
+        playingCard2 = PlayingCard()
+        hasEnded = Bool()
+        buttonClicked = Bool()
+    }
+ 
+    
+    //methods
+    func startGame() -> Void
+    {
+        
+    }
+}
